eb7f0fe13c756e048f35304b1b39bc461fc8455c
jheg <jason@jheg.co.uk>
2015-01-30 13:02:31 +0000

    migration file error fix

--- db/migrate/20150103235640_add_timestamps_to_users.rb
+++ db/migrate/20150103235640_add_timestamps_to_users.rb
@@ -1,6 +1,5 @@
 class AddTimestampsToUsers < ActiveRecord::Migration
   def change
-    remove_column :users, :created_at, :datetime
     add_column :users, :created_at, :datetime
     add_column :users, :updated_at, :datetime
   end
